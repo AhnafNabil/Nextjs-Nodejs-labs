3b76a73129eed192b55e7577aabe48b68ecf0179
Fazlul Karim <fazlulkarim362@gmail.com>
2024-07-17 14:42:17 +0600

    docker-problem-sovled

--- app/client/app/server/page.tsx
+++ app/client/app/server/page.tsx
@@ -1,17 +1,13 @@
-
 'use client';
 
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-// const serverUrl = process.env.REACT_APP_SERVER_URL 
-
 export default function FetchData() {
   const [data, setData] = useState<string | null>(null);
 
   useEffect(() => {
-    // axios.get(`${serverUrl}`)
-    axios.get('http://<ec2-instance-public-IP>:5000/data')
+    axios.get('http://host.docker.internal:5000/data')
       .then(response => {
         setData(response.data.message); // Assuming the response has a message property
       })
@@ -21,12 +17,12 @@ export default function FetchData() {
   }, []);
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900 p-4">
       <h1 className="text-4xl font-bold text-blue-500 mb-4">Data from Node.js Server</h1>
       {data ? (
         <p className="bg-white dark:bg-gray-800 dark:text-white p-4 rounded-lg shadow-md text-left">{data}</p>
       ) : (
-        <p className="text-gray-500">Loading...</p>
+        <p className="text-gray-500 dark:text-gray-400">Loading...</p>
       )}
     </div>
   );
